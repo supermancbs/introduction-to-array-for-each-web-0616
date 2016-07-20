9e94b8b861f29646627f08958a071fb0cdc1e38f
supermancbs <clark.sacktor@gmail.com>
2016-07-20 17:50:14 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
